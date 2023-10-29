271cdbe2711a60d7398e261e6c7562790c5c6f8f
meevaldas <meevadas13@gmail.com>
2023-10-30 00:10:13 +0530

    chore: unfinished code

--- subtitle_extractor/urls.py
+++ subtitle_extractor/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from subtitle_extractor.views import home, redirect_with_id
+from subtitle_extractor.views import home, redirect_with_id, details
 
 
 app_name = "subtitle_extractor"
 
 urlpatterns = [
-    path('', home, name="home"),
+    path('', home),
     path('video/', redirect_with_id),
+    path('video/<str:id>', details)
     # path('id/subtitle/', views.extract_subtitles)
 ]
